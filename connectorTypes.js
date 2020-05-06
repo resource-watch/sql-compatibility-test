71cf8dc7291bebbce22a1ca724e7cd9b2a548970
Henrique Pacheco <henrique.pacheco@vizzuality.com>
2020-05-06 19:03:29 +0100

    Add docs for the fields of a connector type

--- connectorTypes.js
+++ connectorTypes.js
@@ -1,18 +1,43 @@
 module.exports = [
     {
+        // The name of the connector type
         connectorType: 'carto',
+
+        // The ID of the dataset to be used
         datasetId: '0b9f0100-ce5b-430f-ad8f-3363efa05481',
+
+        // The table name of the dataset to be used
         tableName: 'edi',
+
+        // The slug of the dataset to be used
         slug: 'Environmental-Democracy-Index-1490086842552',
+
+        // A numeric column from the data of the dataset that can be aggregated
         aggregateColumn: 'AVG(overall_score)',
+
+        // A string column from the data of the dataset
         stringColumn: 'region',
-        numericColumn: 'overall_score',
-        numericColumn2: 'justice_pillar_score',
-        whereLowerValue: 2,
-        whereEqValue: 2.1,
-        whereGreaterValue: 2.2,
-        whereLowerValue2: 1,
+
+        // An example of a value for the stringColumn in the data of the dataset
         whereLikeValue: '\'Europ%\'',
+
+        // A numeric column from the data of the dataset
+        numericColumn: 'overall_score',
+
+        // A second numeric column from the data of the dataset
+        numericColumn2: 'justice_pillar_score',
+
+        // There should be at least one row in the data where numericColumn > whereLowerValue
+        whereLowerValue: 2,
+
+        // There should be at least one row in the data where numericColumn = whereEqValue
+        whereEqValue: 2.1,
+
+        // There should be at least one row in the data where numericColumn < whereGreaterValue
+        whereGreaterValue: 2.2,
+
+        // There should be at least one row in the data where numericColumn2 > whereLowerValue2
+        whereLowerValue2: 1,
     },
     {
         connectorType: 'document',
